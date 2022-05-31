7ebdd7a799f73891fd3eb8121e8c72454984618d
sunnysavita10 <engsunny3365@gmail.com>
2022-05-31 22:48:23 +0530

    final update

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="docker-heroku",
-    version="0.0.3",
-    description="ML project",
-    author="Avnish yadav", 
-    packages=find_packages(),
-    license="MIT"
-)
+from flask import Flask
+
+from wsgiref import simple_server
+
+from flask import Flask, session, request, Response, jsonify
+
+
+
+import atexit
+import uuid
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Flask app is running and I am changing something."
+
+port = int(os.getenv("PORT", 5001))
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    #app.run()
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
